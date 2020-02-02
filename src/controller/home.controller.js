248db098a8b496b74621727591d9c83abfb7a3b9
Pedro Zuñiga <pedro.zuniga9749@gmail.com>
2020-02-02 12:01:56 -0500

    Hola

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -52,7 +52,7 @@ function main() {
   console.log("esto es el main");
 
   console.log("esto es el main");
-
+  Walter Renato Ravelo JimenezWalter Renato Ravelo Jimenezv
   console.log("esto es el main");
 
   console.log("esto es el main");
